cb7783495cd72cefd408733fb89addb6b8b0b5c3
Anudeep <155814201+palemanudeep@users.noreply.github.com>
2024-09-20 21:30:41 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "ec2_key_pair" {
   description = "The name of the EC2 key pair for SSH access"
   type        = string
-  default     = "medusa-ec2-terraform"
+  default     = "new"
 }
 
 variable "aws_region" {
