4160fb2583b7672a8171b223af6c2f8bf0db8f6d
Anudeep <155814201+palemanudeep@users.noreply.github.com>
2024-09-21 09:34:06 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,10 +13,10 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket         = "new-medusa-task5-terraform-state-bucket"
-    key            = "new-medusa-terraform-automation.tfstate"
+    bucket         = "medusa-task5-terraform-state-bucket"
+    key            = "medusa-terraform-automation.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "new-terraform-lock-table"
+    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
